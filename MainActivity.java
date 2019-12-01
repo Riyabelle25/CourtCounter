9547bb30ec0c26f297a9cf9b0d3dff71e59cc051
Riyabelle25 <55790848+Riyabelle25@users.noreply.github.com>
2019-12-01 09:50:12 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.courtcounter;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Increase  scoreTeamA 1 point.
+     */
+    public void addOneForTeamA(View v) {
+        displayForTeamA(1);
+    }
+
+    /**
+     * Increase scoreTeamA 2 points.
+     */
+    public void addTwoForTeamA(View v) {
+        displayForTeamA(2);
+    }
+
+    /**
+     * Increase  scoreTeamA 3 points.
+     */
+    public void addThreeForTeamA(View v) {
+        displayForTeamA(3);
+    }
+
+    /**
+     * Displays scoreTeamA.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+}
